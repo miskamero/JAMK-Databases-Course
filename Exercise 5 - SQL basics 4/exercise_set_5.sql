8e1f48396453b2bad39fe272ead8cd4af3a65540
miskamero <miska.mero@gmail.com>
2024-10-28 23:52:25 +0200

    ex5 done

---
+++ Exercise 5 - SQL basics 4/exercise_set_5.sql
@@ -0,0 +1,157 @@
+-- ====== Exercise 5 - SQL Basics 4 ======
+
+-- 1 | Create a new table called offices with the following columns:
+--     - OfficeID (primary key)
+--     - OfficeName
+--     - Address
+--     - PostalCode
+--     - City
+
+-- --------------------------------------------------
+CREATE TABLE offices (
+    OfficeID INT PRIMARY KEY,
+    OfficeName VARCHAR(70),
+    Address VARCHAR(255),
+    PostalCode VARCHAR(15),
+    City VARCHAR(40)
+);
+-- --------------------------------------------------
+
+-- ##################################################
+-- 2 | Create a connection between offices and employees tables. Each employee can have one office attached (this should be an optional field). Use the following foreign key options:
+--     - UPDATE CASCADE
+--     - DELETE NO ACTION
+-- Important: If you get an error (code 1452), run the following command for the current session before connection creation:
+--     - SET FOREIGN_KEY_CHECKS=0;
+-- --------------------------------------------------
+ALTER TABLE employees
+ADD COLUMN OfficeID INT,
+ADD CONSTRAINT fk_office
+    FOREIGN KEY (OfficeID) REFERENCES offices(OfficeID)
+    ON UPDATE CASCADE
+    ON DELETE NO ACTION;
+-- --------------------------------------------------
+
+-- ##################################################
+-- 3 | Now do the following modifications to offices table:
+-- 	- Add automatic counter for the OfficeID column so that new value will be generated automatically each time new data is inserted.
+-- 	- Add a new column: manager (use data type int)
+-- 	- Create another connection between offices and employees tables so that each office will have one employee working as a manager.
+-- --------------------------------------------------
+ALTER TABLE offices
+MODIFY COLUMN OfficeID INT AUTO_INCREMENT;
+ALTER TABLE offices
+ADD COLUMN manager INT;
+ALTER TABLE offices ADD CONSTRAINT fk_manager
+    FOREIGN KEY (manager) REFERENCES employees(EmployeeID)
+    ON UPDATE CASCADE
+    ON DELETE NO ACTION;
+-- --------------------------------------------------
+
+-- ##################################################
+-- 4 | Create input validation check using TRIGGER to offices table. OfficeName field value should not begin with a word office (for example, officenorth should not be accepted while northoffice will be accepted).
+-- --------------------------------------------------
+DELIMITER //
+CREATE TRIGGER check_office_name
+BEFORE INSERT ON offices
+FOR EACH ROW
+BEGIN
+    IF LOWER(LEFT(NEW.OfficeName, 6)) = 'office' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Office name cannot start with word office';
+    END IF;
+END;
+//
+-- --------------------------------------------------
+
+-- ##################################################
+-- 5 | Create a new table called warehouses with the following columns:
+-- 	- WarehouseID (primary key, numerical value with auto increment feature)
+-- 	- Address
+-- 	- PostalCode
+-- 	- City
+-- 	- SurfaceArea (number with one decimal in square meters)
+-- --------------------------------------------------
+CREATE TABLE warehouses (
+    WarehouseID INT PRIMARY KEY AUTO_INCREMENT,
+    Address VARCHAR(255),
+    PostalCode VARCHAR(20),
+    City VARCHAR(100),
+    SurfaceArea DECIMAL(10, 1)
+);
+
+-- --------------------------------------------------
+
+-- ##################################################
+-- 6 | Connect warehouses table to suppliers table. Each supplier can have many warehouses for supplies, but each warehouse can only be used by one supplier. Use the same foreign key options as previously:
+--     - UPDATE CASCADE
+--     - DELETE NO ACTION
+-- --------------------------------------------------
+ALTER TABLE warehouses
+ADD COLUMN SupplierID INT,
+ADD CONSTRAINT fk_supplier
+    FOREIGN KEY (SupplierID) REFERENCES suppliers(SupplierID)
+    ON UPDATE CASCADE
+    ON DELETE NO ACTION;
+-- --------------------------------------------------
+
+-- ##################################################
+-- 7 | Add the following columns to warehouses table:
+-- 	- LastUpdated (automatic datetime value when new data is inserted or existing data is updated)
+-- 	- AdditionalInfo (should store large amount of text data)
+-- --------------------------------------------------
+ALTER TABLE warehouses
+ADD COLUMN LastUpdated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ADD COLUMN AdditionalInfo TEXT;
+-- --------------------------------------------------
+
+-- ##################################################
+-- 8 | Create a table called storages. Suppliers will store the following information about products into this table:
+-- 	- StorageID (integer with auto increment feature)
+-- 	- Shelf (shelf identifier, ie. A1, A2 etc.)
+-- 	- Quantity (number of product units that have been stored)
+-- --------------------------------------------------
+CREATE TABLE storages (
+    StorageID INT PRIMARY KEY AUTO_INCREMENT,
+    Shelf VARCHAR(5),
+    Quantity INT
+)
+-- --------------------------------------------------
+
+-- ##################################################
+-- 9 | Connect the storages table with products and warehouses tables. One row in storages table will tell how many units of what product is stored in which warehouse (and of course the specific location in that warehouse created earlier with shelf identifier). Use a primary key column StorageID for this table with integer data type and auto increment feature.
+-- --------------------------------------------------
+ALTER TABLE storages
+ADD COLUMN ProductID INT,
+ADD COLUMN WarehouseID INT,
+ADD CONSTRAINT fk_product
+    FOREIGN KEY (ProductID) REFERENCES products(ProductID)
+    ON UPDATE CASCADE
+    ON DELETE NO ACTION,
+ADD CONSTRAINT fk_warehouse
+    FOREIGN KEY (WarehouseID) REFERENCES warehouses(WarehouseID)
+    ON UPDATE CASCADE
+    ON DELETE NO ACTION;
+-- --------------------------------------------------
+
+-- ##################################################
+-- 10 | Create input validation check using TRIGGER to storages table. The following two conditions should be check before data is inserted:
+-- 	- Quantity cannot be negative and cannot exceed the value of 5000000.
+-- 	- Shelf identifiers must be in format XY where X can only be a letter in range A-Z inclusive and Y a number in range 0-9 inclusive (for example, AX1 should not be accepted).
+-- --------------------------------------------------
+DELIMITER //
+CREATE TRIGGER validate_storage_input
+BEFORE INSERT ON storages
+FOR EACH ROW
+BEGIN
+    IF NEW.Quantity < 0 OR NEW.Quantity > 5000000 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Quantity must be between 0 and 5,000,000.';
+    END IF;
+    IF NOT (NEW.Shelf REGEXP '^[A-Z][0-9]$') THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Shelf must be in the format "XY", where X is A-Z and Y is 0-9.';
+    END IF;
+END;
+//
+-- --------------------------------------------------
---
+++ Exercise 6 - SQL basics 5/exercise_set_6.sql
@@ -0,0 +1,82 @@
+-- ====== Exercise 6 - SQL Basics 5 ======
+
+-- 1 | Create the following offices:
+-- 	- Saint Louis office (address: Palm st 16, postalcode: 63107, city: Saint Louis, manager: Robert King)
+-- 	- Springfield office (address: Ohio Ave 3, postalcode: 62702, city: Springfield, manager: Nancy Davolio)
+-- 	- Kansas City office (address: Beverly st 80, postalcode: 66204, city: Kansas City, manager: Adam West)
+-- --------------------------------------------------
+INSERT INTO offices (OfficeID, OfficeName, Address, PostalCode, City)
+VALUES (1, 'Saint Louis', 'Palm st 16', 63107, 'Saint Louis'),
+       (2, 'Springfield', 'Ohio Ave 3', 62702, 'Springfield'),
+       (3, 'Kansas City', 'Beverly st 80', 66204, 'Kansas City');
+-- --------------------------------------------------
+
+-- ##################################################
+-- 2 | Create the following warehouses:
+-- 	- Liberty warehouse (address: Grant Ave 400, postalcode: 64068, city: Liberty, surfacearea: 200m2, supplier: New Orleans Cajun Delights)
+-- 	- Blue Springs warehouse (address: 1030 SE Forest Ridge Ct, postalcode: 64014, city: Blue Springs, surfacearea: 350m2, supplier: Tasty Roots)
+-- 	- Eldon warehouse (address: 800 W Champain St, postalcode: 65026, city: Eldon, surfacearea: 400m2, supplier: New Orleans Cajun Delights)
+-- 	- Quincy warehouse (address: 3100 Payson Rd, postalcode: 62305, city: Quincy, surfacearea: 280m2, supplier: Tropical Fruits)
+-- 	- Cameron warehouse (address: 650 E Grand Ave, postalcode: 64429, city: Cameron, surfacearea: 310m2, supplier: Bigfoot Breweries)
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+
+-- ##################################################
+-- 3 | Let's add couple of products into some of previously created warehouses (more specifically into storage shelves inside warehouses). Add the following products:
+-- 	- Products in Liberty Warehouse (Grant Ave 400):
+-- 		* product: Chang, quantity: 16000, shelf: A7
+-- 		* product: Tofu, quantity: 12000, shelf: T8
+-- 	- Products in Blue Springs Warehouse (1030 SE Forest Ridge Ct):
+-- 		* product: Maxilaku, quantity: 50000, shelf: B5
+-- 		* product: Ipoh Coffee, quantity: 35000, shelf: C9
+-- 	- Products in Eldon Warehouse (800 W Champain St):
+-- 		* product: Sun-Dried Tomatoes, quantity: 95000, shelf: A1
+-- 		* product: Almond Milk, quantity: 15000, shelf: Q7
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+
+-- ##################################################
+-- 4 | Springfield office moves to a new address in Springfield. Do the following changes:
+-- 	- address: 1500 Knotts St
+-- 	- postalcode: 62703
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+
+-- ##################################################
+-- 5 | Manager of Kansas City office has left the company and Laura Callahan will be the new manager. Do the update using subquery (tip: get the employeeid for manager column using subquery)!
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+
+-- ##################################################
+-- 6 | Double the quantity of Tofu and move the product to W8 shelf which has more space for the greater quantity in Liberty Warehouse.
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+
+-- ##################################################
+-- 7 | Create a copy of the storages table. New table should be called storages_backup.
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+
+-- ##################################################
+-- 8 | Copy the data from storages table into storages_backup table.
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+
+-- ##################################################
+-- 9 | The lease for the warehouse in Liberty is coming to an end and the supplier plans to move the products to a larger warehouse in Eldon (Do this same update into the storages_backup table too!). Transfer the Liberty warehouse products to Eldon and remove the Liberty warehouse from the warehouses table.
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+
+-- ##################################################
+-- 10 | Remove all products from storages table with 0 as a quantity value.
+-- --------------------------------------------------
+
+-- --------------------------------------------------
